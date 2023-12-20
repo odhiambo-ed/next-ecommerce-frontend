89a8bdb25ee3a83f10aa76fe0ee69d2d18f9abcb
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-20 14:25:27 +0300

    add hero to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import HeroSlider from '@/components/HeroSlider'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Shopping Mall</h1>
+     <HeroSlider />
     </main>
   )
 }
