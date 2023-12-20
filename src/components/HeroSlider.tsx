6e74d3cf8a899222f08cb4924c991e209c1d368a
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-20 14:24:35 +0300

    add hero slider component

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -1,8 +1,64 @@
+'use client'
+
 import React from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slide from './Slide'
 
 function HeroSlider() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        arrows: false,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+    };
+
+    const slideData = [
+        {
+        id: 0,
+        img: "/images/banner-1.jpg",
+        title: "Trending Item",
+        mainTitle: "WOMEN'S LATEST FASHION SALE",
+        price: "$20",
+        },
+        {
+        id: 1,
+        img: "/images/banner-2.jpg",
+        title: "Trending Accessories",
+        mainTitle: "MODERN SUNGLASSES",
+        price: "$15",
+        },
+        {
+        id: 2,
+        img: "/images/banner-3.jpg",
+        title: "Sale Offer",
+        mainTitle: "NEW FASHION SUMMER SALE",
+        price: "$30",
+        },
+        ];
+    
   return (
-    <div>HeroSlider</div>
+      <div>
+      <div className="container pt-6 lg:pt-0">
+        <Slider {...settings}>
+          {slideData.map((item) => (
+            <Slide
+              key={item.id}
+              img={item.img}
+              title={item.title}
+              mainTitle={item.mainTitle}
+              price={item.price}
+            />
+          ))}
+        </Slider>
+      </div>
+    </div>
   )
 }
 
