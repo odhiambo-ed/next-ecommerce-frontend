b1d9c955a245ada80b2bbb7768826e0fd1a450ba
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-20 13:38:53 +0300

    add menu items to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+      <div className="hidden lg:block">
+          <div className="container">
+              <div className="flex gap-4 text-blackish font-medium w-fit py-4 mx-auto">
+                  <Link className='relative hover:text-accent' href='#'>
+                        Home
+                  </Link>
+                  <Link className='relative hover:text-accent' href='#'>
+                        About Us
+                  </Link>
+                  <Link className='relative hover:text-accent' href='#'>
+                        Service
+                  </Link>
+                  <Link className='relative hover:text-accent' href='#'>
+                        Contact Us
+                  </Link>
+              </div>
+          </div>
+    </div>
   )
 }
 
