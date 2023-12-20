dc9ceaeb7aa519241c7ec591f2237f8ac53bb182
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-20 14:56:27 +0300

    add new product to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import HeroSlider from '@/components/HeroSlider'
+import NewProducts from '@/components/NewProducts'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <HeroSlider />
+      <HeroSlider />
+      <NewProducts />
     </main>
   )
 }
