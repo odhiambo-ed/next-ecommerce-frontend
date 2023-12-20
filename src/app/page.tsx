e1d6d402e7e67097f5a7990efe3c25418c874bb6
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-20 15:01:49 +0300

    add reviews to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
 import HeroSlider from '@/components/HeroSlider'
 import NewProducts from '@/components/NewProducts'
+import Reviews from '@/components/Reviews'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <HeroSlider />
       <NewProducts />
+      <Reviews />
     </main>
   )
 }
